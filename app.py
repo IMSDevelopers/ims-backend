15f1f622467c0a0c6f1397a23f72f5dcef0e813c
Jacob Santos <jacob@jax2.net>
2021-11-08 17:09:27 -0800

    Get users and singular item

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request, make_response
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
@@ -9,12 +10,36 @@ CORS(app)
 def home():
     return "Hello, Flask!"
 
-@app.route("/api/getItems")
-def get_items():
-    items = {
-        "description": "Bolt",
-        "quantity": 8,
-        "url": "https://google.com/"
-    }
-    return jsonify(items)
+@app.route('/api/getItemDatabase')
+def getItem():
+    query = "SELECT * from items where items.id=" + request.args.get('id')
+    cnx = mysql.connector.connect(user='root', password="", database="")
+    cursor = cnx.cursor(dictionary=True)
 
+    cursor.execute(query)
+
+    result = []
+
+    for row in cursor:
+        print(row)
+        result.append(row)
+
+    cursor.close()
+    cnx.close()
+    return jsonify(result)
+
+@app.route("/api/getUsers")
+def get_users():
+    cnx = mysql.connector.connect(user='root', password="cream", database='ims')
+    cursor = cnx.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM users")
+
+    result = []
+
+    for row in cursor:
+        result.append(row)
+
+    cursor.close()
+    cnx.close()
+    return jsonify(result)
